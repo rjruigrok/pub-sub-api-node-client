dcf71f660821686c1c084dfb6138c898f1fab7ce
Rob Ruigrok <robruigrok@gmail.com>
2023-09-07 11:58:35 +0200

    Hardcoded topicname to use as workaround

--- dist/client.js
+++ dist/client.js
@@ -633,6 +633,9 @@ var PubSubApiClient = class {
    * @returns {Promise<Schema>} Promise holding parsed event schema
    */
   async #fetchEventSchemaWithClient(topicName) {
+    // CP: Workaround to make channel /data/FilteredChannel__chn to fetch the right schema.
+    topicName = { topicName: '/data/ContactChangeEvent' };
+
     return new Promise((resolve, reject) => {
       this.#client.GetTopic({ topicName }, (topicError, response) => {
         if (topicError) {
